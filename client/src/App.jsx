84feb86b073007b760567153f927614e8a652983
DeepakshiGaur <150045734+DeepakshiGaur@users.noreply.github.com>
2025-02-22 11:54:04 +0530

    complete create a post page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,20 +10,25 @@ import Signup from './pages/SignUp';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
+import CreatePost from './pages/CreatePost';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
     <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<Signup />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<Signup />} />
         <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Route>
-        <Route path="/projects" element={<Projects />} />
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+        </Route>
+        <Route path='/projects' element={<Projects />} />
       </Routes>
       <Footer />
     </BrowserRouter>
